71deb5945ebb581f095d3e45221b029b3c5166fb
Kamil <kamil2000.sz@gmail.com>
2023-01-21 19:43:31 +0100

    Dodaj pliki projektów.

---
+++ SimpleTodo/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleTodo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodo.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Project> tasks = new () {
+                new() { ID = 1, Name="Projekt 1", Todos = new List<Todo>() },
+                new() { ID = 2, Name = "Projekt 2", Todos = new List<Todo>() },
+                new() { ID = 3, Name = "Projekt 3", Todos = new List<Todo>() },
+            };
+            return View(new ProjectListViewmodel() { Projects = tasks});
+        }
+    }
+}
---
+++ SimpleTodo/Models/ProjectListViewmodel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodo.Models
+{
+    public class ProjectListViewmodel
+    {
+        public ICollection<Project> Projects;
+    }
+}
